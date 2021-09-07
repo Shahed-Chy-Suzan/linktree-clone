8324cbcf166e9435dc5fb56b6e4a46d742a14058
Shahed Chy Suzan <shahedchysuzan@gmail.com>
2021-09-07 20:58:20 +0600

    auth+controller

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,34 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+//linktree.com/dashboard
+Route::group(['middleware' => 'auth', 'prefix' => 'dashboard'], function() {
+
+    //linktree.com/dashboard/links
+    Route::get('/links', 'LinkController@index');
+    Route::get('/links/new', 'LinkController@create');
+    Route::post('/links/new', 'LinkController@store');
+    Route::get('/links/{link}', 'LinkController@edit');
+    Route::post('/links/{link}', 'LinkController@update');
+    Route::delete('/links/{link}', 'LinkController@destroy');
+
+    Route::get('/settings', 'UserController@edit');
+    Route::post('/settings', 'UserController@update');
+
+});
+
+//linktree.com/visit/
+Route::post('/visit/{link}', 'VisitController@store');
+//linktree.com/username
+Route::get('{user}', 'UserController@show');
+
+Route::get('/home', 'HomeController@index')->name('home');
