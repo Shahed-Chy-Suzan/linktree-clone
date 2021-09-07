e8ece461ceec1c734450e180e593bc363fed81d6
Shahed Chy Suzan <shahedchysuzan@gmail.com>
2021-09-08 01:44:36 +0600

    User show

---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6 offset-md-3">
+                @foreach($user->links as $link)
+                    <div class="link">
+                        <a
+                            class="user-link d-block p-4 mb-4 rounded h3 text-center"
+                            style="border: 2px solid {{ $textColor }}; color: {{ $textColor }}"
+                            href="{{ $link->link }}"
+                            target="_blank"
+                            rel="nofollow"
+                            data-link-id="{{ $link->id }}"
+                        >
+                            {{ $link->name }}
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+@endsection
